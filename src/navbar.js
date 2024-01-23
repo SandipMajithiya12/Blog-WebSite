4c7af65636a5d237590da5f0a3610ccc838e6a97
SandipMajithiya12 <sandipmajithiya10@gmail.com>
2024-01-23 19:26:18 +0530

    'final'

---
+++ src/navbar.js
@@ -0,0 +1,17 @@
+import {Link} from 'react-router-dom';
+
+const Navbar = () => {
+
+    return ( 
+        <nav className="navbar">
+            <h1>Sandip Blogs</h1>
+            <div className="links">
+            <Link to="/">home</Link>
+            <Link to="/create">New Blog</Link>
+            </div>
+        </nav>
+       
+     );
+}
+ 
+export default Navbar;
